ad8fa4e9f1ba422f69444844199e354701f72d72
Khoaanh10 <khoaanh15111997@gmail.com>
2021-06-07 23:03:28 +0700

    7/6/2021/23-03

--- ShopKA/ShopKA/obj/Release/Package/PackageTmp/Scripts/App.js
+++ ShopKA/ShopKA/obj/Release/Package/PackageTmp/Scripts/App.js
@@ -696,3 +696,17 @@ $("#demo-5").click(function () {
         mode: "dark",
     })
 });
+
+text_truncate = function (str, length, ending) {
+    if (length == null) {
+        length = 100;
+    }
+    if (ending == null) {
+        ending = '...';
+    }
+    if (str.length > length) {
+        return str.substring(0, length - ending.length) + ending;
+    } else {
+        return str;
+    }
+};
